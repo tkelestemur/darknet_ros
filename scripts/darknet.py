403952378d1f427cd911863b3b83936cc8f1c720
naokiyokoyama <naoki.yokoyama7@gmail.com>
2018-04-09 12:13:38 -0400

    fixed ref to .so

--- scripts/darknet.py
+++ scripts/darknet.py
@@ -43,7 +43,7 @@ class METADATA(Structure):
                 ("names", POINTER(c_char_p))]
 
     
-lib = CDLL("cfg/libdarknet.so", RTLD_GLOBAL)
+lib = CDLL("libdarknet.so", RTLD_GLOBAL)
 
 lib.network_width.argtypes = [c_void_p]
 lib.network_width.restype = c_int
